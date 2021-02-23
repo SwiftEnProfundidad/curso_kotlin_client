896b358351be3315f9f5198308773b4fa4bb6544
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-02-23 19:42:06 +0100

    Fix error

--- src/main/kotlin/Variables.kt
+++ src/main/kotlin/Variables.kt
@@ -3,8 +3,8 @@
 
 fun variables() {
     var monstruoCalavera: Int = -5
-    monstruo = 8
-    monstruo = 1000000
+    monstruoCalavera = 8
+    monstruoCalavera = 1000000
     var variableNumber: Int = 42
 
     var monstruosCalavera = 45
