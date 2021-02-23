061af362f1f28281e99ca8e991f872f61d0a3ed9
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-02-23 20:19:56 +0100

    Finished Theme 02

---
+++ src/main/kotlin/IncrementDecrement.kt
@@ -0,0 +1,15 @@
+
+fun incrementDecrement() {
+
+    // incremento y decremento de variables
+    var monstruoCalavera: Int = 5
+
+    monstruoCalavera = monstruoCalavera + 8
+    monstruoCalavera += 8
+    print(monstruoCalavera)
+
+    // Imprimimos resultados
+    print(monstruoCalavera)
+
+    var monstruosCalavera = 45
+}
