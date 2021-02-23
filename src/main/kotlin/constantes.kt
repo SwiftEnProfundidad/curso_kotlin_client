e8d3a5972addd569c8f3f700158357b4fa9ae884
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-02-23 18:40:50 +0100

    New change

--- src/main/kotlin/constantes.kt
+++ src/main/kotlin/constantes.kt
@@ -2,8 +2,9 @@
 const val reallyConstant: Int = 42
 fun constants() {
  val number: Int = 10
-    val pi: Double = 3.1415
+    val pi: String = "dfasdf"
     val primo2 = 23
 
+    print(number)
     print(number)
 }
