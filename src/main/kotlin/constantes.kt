ae7e8a786247b770cd43ac94e6ca20fc515f8e5f
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-02-24 18:56:20 +0100

    Setup project with Android Studio

--- src/main/kotlin/constantes.kt
+++ src/main/kotlin/constantes.kt
@@ -1,10 +1,9 @@
 
-const val reallyConstant: Int = 42
-fun constants() {
- val number: Int = 10
+fun main() {
+    val number: Int = 10
     val pi: String = "dfasdf"
     val primo2 = 23
 
-    print(number)
+
     print(number)
 }
