8f60f2819971325618c0ff4a2c1cf0a2bac4e7ee
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-02-24 19:17:15 +0100

    Starter operadores con tipos mixtos

--- src/main/kotlin/constantes.kt
+++ src/main/kotlin/constantes.kt
@@ -1,9 +1,23 @@
 
 fun main() {
+
+    // Inferencia de tipos
     val number: Int = 10
     val pi: String = "dfasdf"
     val primo2 = 23
+    //print(number)
 
+    // Conversion de tipos
+    var integer: Int = 100
+    var integer02: Int = 200
+    var decimal: Double = 12.5
 
-    print(number)
+    integer = decimal.toInt()
+
+    var monstruosPantalla01: Int = 100
+    var moscasVoladoraTriple: Double = 30.55
+
+    monstruosPantalla01 = moscasVoladoraTriple.toInt()
+    print(monstruosPantalla01)
+
 }
