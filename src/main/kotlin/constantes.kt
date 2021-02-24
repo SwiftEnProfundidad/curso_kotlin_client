9b540e5092dc52d7465103fc3ab8988aa0627d5a
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-02-24 19:42:43 +0100

    Starter String

--- src/main/kotlin/constantes.kt
+++ src/main/kotlin/constantes.kt
@@ -2,22 +2,37 @@
 fun main() {
 
     // Inferencia de tipos
+
     val number: Int = 10
     val pi: String = "dfasdf"
     val primo2 = 23
     //print(number)
 
     // Conversion de tipos
-    var integer: Int = 100
+
+    var integer = 100
     var integer02: Int = 200
-    var decimal: Double = 12.5
+    var decimal = 12.5
 
     integer = decimal.toInt()
 
-    var monstruosPantalla01: Int = 100
+    var monstruosPantalla01: Int = 10
     var moscasVoladoraTriple: Double = 30.55
 
     monstruosPantalla01 = moscasVoladoraTriple.toInt()
-    print(monstruosPantalla01)
+   // print()
+
+    // Operadores con tipos mixtos
+
+    val hourlyRate = 19.5
+    val hoursWorked = 10
+
+    val TranformhoursWorked = 10.toDouble()
+    val totalCost = hourlyRate * hoursWorked.toDouble()
+    val typeInferredDouble = 3.14159
+
+    val totalCost02 = hourlyRate * hoursWorked
+    print(totalCost02)
+
 
 }
