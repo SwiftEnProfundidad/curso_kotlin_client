40492f2038671a4cf0ee83898f0eb9ed1cb4b78b
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-03-02 19:04:08 +0100

    Starter with pairs & triples

--- src/main/kotlin/Tema03/TiposYOpersaciones.kt
+++ src/main/kotlin/Tema03/TiposYOpersaciones.kt
@@ -34,14 +34,14 @@ fun main() {
     val typeInferredDouble = 3.14159
 
     val totalCost02 = hourlyRate * hoursWorked
-    print(totalCost02)
+    //print(totalCost02)
 
     val stringDog = "Dog"
 
     var message = "Hello" + " my name is "
     val name = "Joe"
     message += name // "Hello my name is Joe"
-    print(message)
+    //print(message)
 
     val exclamationMark: Char = '!'
     message += exclamationMark // "Hello my name is Joe!"
@@ -53,10 +53,55 @@ fun main() {
     val oneThird = 1.0 / 3.0
     val oneThirdLongString = "One third is $oneThird as a decimal."
 
-    print(oneThirdLongString)
+    //print(oneThirdLongString)
 
     val oneThirdLongString02 = "One third is ${1.0 / 3.0} as a decimal."
-    print(oneThirdLongString02)
+    //print(oneThirdLongString02)
+
+    // Strings Multilinea
+
+    val bigString = """
+  |You can have a string that contains multiple
+  |lines
+  |by
+  |doing this.
+  """.trimMargin()
+    println(bigString)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
