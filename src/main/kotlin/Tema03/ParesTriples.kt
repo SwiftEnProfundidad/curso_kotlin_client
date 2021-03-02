b3d37052dab1b0357e493d45e60ec4d463287401
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-03-02 20:16:48 +0100

    Starter with any, unit & nothing types

--- src/main/kotlin/Tema03/ParesTriples.kt
+++ src/main/kotlin/Tema03/ParesTriples.kt
@@ -1,9 +1,11 @@
 package Tema03
 
+import java.util.function.DoubleBinaryOperator
+
 fun main() {
 
     // Pair o pares
-    val coordinates: Triple<Int, Int, Double> = Triple(2, 3, 7.44)
+    val coordinates: Triple<Int, Int, Double> = Triple(234, 3, 7.44)
 
     var coordinates02 = Triple(12,67,89)
 
@@ -28,4 +30,14 @@ fun main() {
     val (x, _, z) = coordinates02
     // x and y , z ,both inferred to be of type Int & Double
 
+    val pos: Double = 20.1234567891234566666666663563463456345
+
+    val a: Short = 12
+    val b: Byte = 120
+    val c: Int = -100000
+
+    val answer = a + b + c // Answer will be an Int
+
+    print(answer)
 }
+
