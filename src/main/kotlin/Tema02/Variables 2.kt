402b72f312b55fc7717543fa032b34d6f29d6097
Juan Carlos Merlos Albarracin <juan.merlosalbarracin@plexus.es>
2021-03-02 21:11:22 +0100

    Refactor

---
+++ src/main/kotlin/Tema02/Variables 2.kt
@@ -0,0 +1,13 @@
+package Tema02
+
+
+fun variables() {
+    var monstruoCalavera: Int = -5
+    monstruoCalavera = 8
+    monstruoCalavera = 1000000
+    var variableNumber: Int = 42
+
+    var monstruosCalavera = 45
+}
+
+
